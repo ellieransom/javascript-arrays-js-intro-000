d6d85e38fe4c2d3a3207fe3f365ab9b19c323034
ellie.ransom <ellie.ransom@splashthat.com>
2018-12-19 21:10:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+
+
+
 var addElementToBeginningOfArray = (myArray, myElement) => {
   newArray = [myElement, ...myArray]
   return newArray
